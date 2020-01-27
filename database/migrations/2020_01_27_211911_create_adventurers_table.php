ebc3ffe4315f1b9ccf5f1dbfd5cf54c72bbb144c
stevenh <stevenh@hollywoodfotofix.com>
2020-01-27 14:45:50 -0700

    Adventurers table migration

---
+++ database/migrations/2020_01_27_211911_create_adventurers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdventurersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adventurers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->tinyInteger('element');
+            $table->tinyInteger('weapon');
+            $table->tinyInteger('role');
+            $table->tinyInteger('rarity_min');
+            $table->tinyInteger('rarity_max');
+            $table->smallInteger('manaCircle_max');
+            $table->smallInteger('strength_min');
+            $table->smallInteger('strength_max');
+            $table->smallInteger('health_min');
+            $table->smallInteger('health_max');
+            $table->smallInteger('might_min');
+            $table->smallInteger('might_max');
+            $table->string('gamepediaLink')->nullable();
+            $table->string('gamepressLink')->nullable();
+            $table->tinyInteger('limited')->default(0);
+            $table->integer('event')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adventurers');
+    }
+}
