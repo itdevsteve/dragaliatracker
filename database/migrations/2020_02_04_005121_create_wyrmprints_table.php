abe27a50288eb4e560d20df5cb8427c2bb57fab9
Steven Haslam <itdevsteve@gmail.com>
2020-02-03 18:02:11 -0700

    Wyrmprint table migration

---
+++ database/migrations/2020_02_04_005121_create_wyrmprints_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWyrmprintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wyrmprints', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->tinyInteger('rarity_min');
+            $table->tinyInteger('rarity_max');
+            $table->smallInteger('strength_min');
+            $table->smallInteger('strength_max');
+            $table->smallInteger('health_min');
+            $table->smallInteger('health_max');
+            $table->string('gamepediaLink')->nullable();
+            $table->string('gamepressLink')->nullable();
+            $table->tinyInteger('limited')->default(0);
+            $table->integer('event')->nullable();
+            $table->date('releaseDate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wyrmprints');
+    }
+}
