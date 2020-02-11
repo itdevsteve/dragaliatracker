d14eb6d8d352a9258105f569cb8a4f2bb0f9924e
Steven Haslam <itdevsteve@gmail.com>
2020-02-10 19:18:06 -0700

    Events table migration

---
+++ database/migrations/2020_02_11_021039_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name_en');
+            $table->string('name_jp');
+            $table->string('description_en');
+            $table->string('description_jp');
+            $table->string('type');
+            $table->string('gamepediaLink')->nullable();
+            $table->string('gamepressLink')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
